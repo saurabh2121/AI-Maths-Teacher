b4742ea636e20fe7c36ff769b01798ebea97f38d
SAURABH YADAV <102783060+saurabh2121@users.noreply.github.com>
2025-03-01 00:31:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let btn=document.querySelector("button")
 let text=document.querySelector("#text")
 let output=document.querySelector(".output")
 
-const Api_url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyC74UwtFHm2iYnOWD8MznanAFZRuXGz3V8"
+const Api_url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBhUzrNY73tBg1WyI2BqXdRMNFUKXvtCEw"
 
 let fileDetails={
     mime_type:null,
